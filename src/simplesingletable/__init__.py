f3d1c9354886d5f93680689a9b4891cbe2607270
Sully Sullenberger <sully@sadburger.com>
2023-11-06 14:24:03 -0800

    Bump version 1.5.0 -> 1.5.1

--- src/simplesingletable/__init__.py
+++ src/simplesingletable/__init__.py
@@ -23,7 +23,7 @@ class Constants:
     QUERY_DEFAULT_MAX_API_CALLS = 10
 
 
-package_version = "1.5.0"
+package_version = "1.5.1"
 
 AnyDbResource = TypeVar("AnyDbResource", bound=Union[DynamodbVersionedResource, DynamodbResource])
 VersionedDbResourceOnly = TypeVar("VersionedDbResourceOnly", bound=DynamodbVersionedResource)
