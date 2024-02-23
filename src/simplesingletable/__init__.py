89325c97c998a6333906babf19cd9a1b0db4a459
Sully Sullenberger <sully@sadburger.com>
2024-02-23 12:38:14 -0800

    Bump version 2.3.2 -> 3.0.0

--- src/simplesingletable/__init__.py
+++ src/simplesingletable/__init__.py
@@ -23,7 +23,7 @@ class Constants:
     QUERY_DEFAULT_MAX_API_CALLS = 10
 
 
-package_version = "2.3.2"
+package_version = "3.0.0"
 
 AnyDbResource = TypeVar("AnyDbResource", bound=Union[DynamoDbVersionedResource, DynamoDbResource])
 VersionedDbResourceOnly = TypeVar("VersionedDbResourceOnly", bound=DynamoDbVersionedResource)
