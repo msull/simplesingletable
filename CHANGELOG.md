6b9f285433fd900d8d16876bc5647d3088eca0db
Sully Sullenberger <sully@sadburger.com>
2024-03-05 09:50:04 -0800

    update changelog, add utility function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [3.2.0] 2024-03-05
+
+### Added
+
+* Added a new utility function to create the standard dynamodb table, given a dynamodb resource object.
+
+### Changed
+
+* Re-organized the core DynamoDbMemory code into a module (out of `__init__.py`) to enhance logging
+
 ## [3.1.0] 2024-03-01
 
 ### Added
--- src/simplesingletable/utils.py
+++ src/simplesingletable/utils.py
@@ -7,7 +7,7 @@ import ulid
 from boto3.dynamodb.types import TypeSerializer
 
 if TYPE_CHECKING:
-    from mypy_boto3_dynamodb.service_resource import Table
+    from mypy_boto3_dynamodb.service_resource import Table, DynamoDBServiceResource
 
 
 def _now(tz: Any = False):
@@ -39,6 +39,66 @@ def encode_pagination_key(last_evaluated_key: dict) -> str:
 def decode_pagination_key(pagination_key: str) -> dict:
     """Turn the pagination key back into the dynamodb LEK dict."""
     return json.loads(urlsafe_b64decode(pagination_key).decode())
+
+
+def create_standard_dynamodb_table(table_name: str, dynamodb_resource: "DynamoDBServiceResource") -> "Table":
+    # Create the DynamoDB table
+    table = dynamodb_resource.create_table(
+        TableName=table_name,
+        KeySchema=[
+            {"AttributeName": "pk", "KeyType": "HASH"},  # Partition key
+            {"AttributeName": "sk", "KeyType": "RANGE"},  # Sort key
+        ],
+        AttributeDefinitions=[
+            {"AttributeName": "pk", "AttributeType": "S"},
+            {"AttributeName": "sk", "AttributeType": "S"},
+            {"AttributeName": "gsitype", "AttributeType": "S"},
+            {"AttributeName": "gsitypesk", "AttributeType": "S"},
+            {"AttributeName": "gsi1pk", "AttributeType": "S"},
+            {"AttributeName": "gsi2pk", "AttributeType": "S"},
+            {"AttributeName": "gsi3pk", "AttributeType": "S"},
+            {"AttributeName": "gsi3sk", "AttributeType": "S"},
+        ],
+        GlobalSecondaryIndexes=[
+            {
+                "IndexName": "gsitype",
+                "KeySchema": [
+                    {"AttributeName": "gsitype", "KeyType": "HASH"},
+                    {"AttributeName": "gsitypesk", "KeyType": "RANGE"},
+                ],
+                "Projection": {"ProjectionType": "ALL"},
+            },
+            {
+                "IndexName": "gsi1",
+                "KeySchema": [
+                    {"AttributeName": "gsi1pk", "KeyType": "HASH"},
+                    {"AttributeName": "pk", "KeyType": "RANGE"},
+                ],
+                "Projection": {"ProjectionType": "ALL"},
+            },
+            {
+                "IndexName": "gsi2",
+                "KeySchema": [
+                    {"AttributeName": "gsi2pk", "KeyType": "HASH"},
+                    {"AttributeName": "pk", "KeyType": "RANGE"},
+                ],
+                "Projection": {"ProjectionType": "ALL"},
+            },
+            {
+                "IndexName": "gsi3",
+                "KeySchema": [
+                    {"AttributeName": "gsi3pk", "KeyType": "HASH"},
+                    {"AttributeName": "gsi3sk", "KeyType": "RANGE"},
+                ],
+                "Projection": {"ProjectionType": "ALL"},
+            },
+        ],
+        BillingMode="PAY_PER_REQUEST",
+    )
+
+    # Wait for the table to be created
+    table.meta.client.get_waiter("table_exists").wait(TableName=table_name)
+    return table
 
 
 def truncate_dynamo_table(dynamo_table: "Table"):
