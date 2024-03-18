14fc98e2e6190337fe06b05026b0fc822e3cb28e
Sully Sullenberger <sully@sadburger.com>
2024-03-18 12:26:04 -0700

    Bump version 3.2.1 -> 3.3.0

--- src/simplesingletable/__init__.py
+++ src/simplesingletable/__init__.py
@@ -1,6 +1,6 @@
 from .dynamodb_memory import DynamoDbMemory, DynamoDbResource, DynamoDbVersionedResource, PaginatedList
 
-package_version = "3.2.1"
+package_version = "3.3.0"
 
 _ = DynamoDbMemory
 _ = DynamoDbResource
