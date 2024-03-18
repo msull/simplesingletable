29af5f571b5a60d274f7aa1677202ef584fa251e
Sully Sullenberger <sully@sadburger.com>
2024-03-18 14:28:03 -0700

    import fix

--- src/simplesingletable/__init__.py
+++ src/simplesingletable/__init__.py
@@ -1,4 +1,10 @@
-from .dynamodb_memory import DynamoDbMemory, DynamoDbResource, DynamoDbVersionedResource, PaginatedList
+from .dynamodb_memory import (
+    DynamoDbMemory,
+    DynamoDbResource,
+    DynamoDbVersionedResource,
+    PaginatedList,
+    exhaust_pagination,
+)
 
 package_version = "3.3.0"
 
@@ -6,3 +12,4 @@ _ = DynamoDbMemory
 _ = DynamoDbResource
 _ = DynamoDbVersionedResource
 _ = PaginatedList
+_ = exhaust_pagination
