f1e9abeace7b684033da13e4b394be832d999b96
Sully Sullenberger --global <sully@sadburger.com>
2025-10-02 10:14:31 -0700

    Bump version 12.6.0 -> 12.7.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Simple Single Table
 
-**Latest Version:** 12.6.0
+**Latest Version:** 12.7.0
 
 ## Project Overview
 
--- src/simplesingletable/__init__.py
+++ src/simplesingletable/__init__.py
@@ -6,7 +6,7 @@ from .dynamodb_memory import (
     exhaust_pagination,
 )
 
-package_version = "12.6.0"
+package_version = "12.7.0"
 
 _ = DynamoDbMemory
 _ = DynamoDbResource
